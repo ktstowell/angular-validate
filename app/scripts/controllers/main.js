9111f1156869453a801efb381cbf075d27fe3b0d
Ken Stowell <ken@metaltoad.com>
2013-08-29 12:31:42 -0700

    Removing the boilerplate yeoman array and adding teh ngvalidation injection.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('ngValidateApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, ngValidation) {
+    
   });
