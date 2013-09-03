a5369d4e2f07781ab0ab04435d37c03ddb3c7e76
Ken Stowell <ken@metaltoad.com>
2013-09-03 09:45:12 -0700

    Syntax highlighter.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,8 @@
 
 angular.module('ngValidateApp')
   .controller('MainCtrl', function ($scope, ngValidation) {
-    
+  	var code = document.querySelectorAll('code');
+  	for(var i=0, j=code.length; i<j; i++) {
+		hljs.highlightBlock(code[i]);
+  	}
   });
