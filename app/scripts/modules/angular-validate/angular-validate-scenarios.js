209bf75337a1f9a2abdd25524418a80e6c010904
Ken Stowell <ken@metaltoad.com>
2013-09-03 09:50:14 -0700

    Scaffolding for scenarios file.

---
+++ app/scripts/modules/angular-validate/angular-validate-scenarios.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*******************************************************************************************************
+ * ANGULAR VALIDATION RULES
+ *******************************************************************************************************
+ *
+ * @description
+ * @dependencies
+ * @injections
+ * @author Ken Stowell
+ */
+ angular.module('ng-validation').factory('ngValidationScenarios', function() {
+  /*******************************************************************************************************
+   * VALIDATION RULES
+   *******************************************************************************************************
+   * @description
+   * @dependency
+   * @author         
+   */
+  var Scenarios = {
+    
+  };
+
+  // Return the rules object
+  return Scenarios;
+ });
