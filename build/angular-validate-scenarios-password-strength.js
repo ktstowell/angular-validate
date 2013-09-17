6927f5915de752521c5f23493be717fe4bdd9fec
Ken Stowell <ken@metaltoad.com>
2013-09-17 14:25:11 -0700

    Bump to 0.3.3

--- build/angular-validate-scenarios-password-strength.js
+++ build/angular-validate-scenarios-password-strength.js
@@ -26,17 +26,17 @@
       standards: {
         strong: {
           length: 12,
-          ui: 'Could defeat a small clan of ninjas.',
+          ui: 'Strong',
           score: 28
         },
         medium: {
           length: 8,
-          ui: 'Meh',
+          ui: 'Medium',
           score: 15
         },
         weak: {
           length: 5,
-          ui: 'pansy',
+          ui: 'Weak',
           score: 8
         }
       },
@@ -47,15 +47,13 @@
     // Scenario score meter
     this.score;
     // Value point system, totally arbitrary but immutable by user
-    this.values = {length: 1, uppercase: 4, number: 4, special: 6};
+    this.values = {length: 1, uppercase: 3, number: 4, special: 6};
     // Merge options
     this.opts = root.extend(this.defaults, opts);
     // DOM
     this.password = form.querySelectorAll('input[type="password"]')
     // Sate DOM
-    this.strength_ui = (this.opts.ui)? form.querySelectorAll(this.opts.ui)[0] : [];
-    // Criteria tracker
-    this.strengths = {uppercase: 'weak', number: 'weak', special: 'weak'};
+    this.strength_ui = (this.opts.ui)? form.querySelectorAll(this.opts.ui)[0] : false;
     // Root class, passed in as argument
     this.root = root;
     // Set up event bindings
@@ -76,6 +74,8 @@
    */
   PasswordStrength.prototype.check_password_strength = function(el, e) {
     var self = this,
+        keys, // Array-ified version of Standards
+        scores = [], // Array of score
         val = el.value, // String value of dom target
         values = { // Object to hold regex return vals.
           uppercase: 0,
@@ -83,6 +83,7 @@
           special: 0,
           length: 0
         };
+
     // Set the score to 0 in every callback - it's easier than tracking state
     this.score = 0;
     // Number of uppercase characters
@@ -100,17 +101,20 @@
       this.score += (values[value] * this.values[value])
     }
 
-    // Update the UI based on score
-    if(this.score >= this.opts.standards.strong.score) {
-      // Strong or higher
-      this.strength_ui.textContent = this.opts.standards.strong.ui;
-    } else if (this.score >= this.opts.standards.medium.score) {
-      // Medium or higher
-      this.strength_ui.textContent = this.opts.standards.medium.ui;
-    } else if (this.score < this.opts.standards.medium.score) {
-      // Less than medium
-      this.strength_ui.textContent = this.opts.standards.weak.ui;
-    } 
+    // If the standard has a strength UI.
+    if(this.strength_ui) {
+      // Because the standards themselves are muteable, we need to account for an indefinite number
+      // of standards in an unenforceable order.
+      // The first task is to get the scores, and sort them ascending:
+      this.scores = Object.keys(this.opts.standards).map(function(key) {
+        return self.opts.standards[key].score;
+      }).sort(function(a, b) {
+        return a - b;
+      });
+      
+      // Then we call our recursive updater
+      this.show_strength_ui(this.scores);
+    }
 
     // If the score meets the acceptance
     if(this.score >= this.opts.standards[this.opts.acceptance].score) {
@@ -125,6 +129,42 @@
     } else {
       // Score didn't meet acceptance: fail
       this.root.epic_fail(el, false, e);
+    }
+  };
+
+  /**
+   * SHOW STRENGTH UI
+   * @param  {Array} scores Array of scores to compare
+   * @description This function does quite a bit of work. It compares a numerically sorted by ascending
+   *              array to determine what standard UI it can show.
+   */
+  PasswordStrength.prototype.show_strength_ui = function(scores) {
+    var self = this, // scope
+        mid_scores = [], // Array to hold middle values
+        mids_max = Math.max.apply(null, scores), // Highest score in scores
+        mids_min = Math.min.apply(null, scores); // Lowest score in scores
+        
+    // Begin by looping through the standards
+    for(var standard in this.opts.standards) {
+      // If the score is the lowest one
+      if(this.score <= this.opts.standards[standard].score && this.opts.standards[standard].score <= mids_min) {
+        // Update the UI
+        this.strength_ui.textContent = this.opts.standards[standard].ui;
+      } else {
+        // This conditional captures every score that isn't min or max.
+        if(this.score > mids_min && this.score < mids_max && this.opts.standards[standard].score > mids_min && this.opts.standards[standard].score < mids_max) {
+          // Push to the temp array which is used as the recursive set of params.
+          mid_scores.push(this.opts.standards[standard].score);
+          // Recurse
+          this.show_strength_ui(mid_scores);
+        } else {
+          // If it's the highest score
+          if(this.score >= this.opts.standards[standard].score && this.opts.standards[standard].score >= mids_max) {
+            // Update the UI
+            this.strength_ui.textContent = this.opts.standards[standard].ui;
+          }
+        }
+      }
     }
   };
   
